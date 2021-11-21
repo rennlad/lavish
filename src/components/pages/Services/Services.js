c42f4fb96fe2f7a791a5b3ba847283a76577aca0
Rennlad <rennickman@gmail.com>
2021-11-21 00:06:33 +0000

    first

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import HeroSection from '../../HeroSection';
 import Pricing from '../../Pricing';
-import { homeObjOne } from './Data';
+import { homeObjFour } from './Data';
 
 
 function Services() {
-    
+
     return (
         <>
-            <HeroSection {...homeObjOne} />
+            <HeroSection {...homeObjFour} />
         </>
     )
 }
